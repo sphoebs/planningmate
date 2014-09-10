d1679faaee4260b2d13cba844bec9912e06f5bb7
Fabio Casati <fabio.casati@unitn.it>
2014-09-10 13:18:22 +0200

    fsa

--- secrets.py
+++
@@ -1,6 +0,0 @@
-SECRET_SESSION_KEY = '8^=)y1m8(009jz3lf7pj+d7lpds(l@xosj+__b@imo!(4l^4$1'
-
-GOOGLE_CLIENT_SECRET= "KxUZ_gytv_7md83oM21yhOg_"
-
-ADMINS=["fabio.casati@gmail.com","admin@bonjogroup.com", "margomiracle@gmail.com"]
-
